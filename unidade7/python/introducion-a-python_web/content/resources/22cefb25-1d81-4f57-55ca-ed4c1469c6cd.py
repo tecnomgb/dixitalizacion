b35311a5f6138d38c6a7cff0b587f2df68e93b92
tecnomgb <sindo@iesgarciabarros.org>
2026-06-08 01:44:03 +0200

    Actualización tarefas python

---
+++ unidade7/python/introducion-a-python_web/content/resources/22cefb25-1d81-4f57-55ca-ed4c1469c6cd.py
@@ -0,0 +1,23 @@
+
+print('?')
+
+print('Dime o primeiro número')
+
+a = int(?)
+
+print('?')
+
+? = int(?)
+
+print('Elixe a operación a realizar: suma(s), resta(r) ou multiplicación(m)')
+
+operacion = input()
+
+if ? == 's':
+    print('O resultado é', a + b)
+elif ? == 'r':
+    print('?', ?)
+? ? == '?':
+    print('?', ?)
+else:
+    ?('?')
---
+++ unidade7/python/introducion-a-python_web/content/resources/7a717692-cae4-2fa6-1c10-ba2766527d44.py
@@ -0,0 +1,12 @@
+
+print('?')
+
+? = int()
+
+if ? > ?: #caso entre 5 e 30
+    if ? < ?:
+        print('?') #caso entre 5 e 30
+    else:
+        print('?') #caso maior de 30
+?:
+    ?('?') #caso menor que 5
