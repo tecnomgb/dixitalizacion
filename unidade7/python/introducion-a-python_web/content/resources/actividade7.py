8372a6aad1d75239ada695be2d75e8174a699149
tecnomgb <sindo@iesgarciabarros.org>
2026-06-08 16:21:18 +0200

    actulización actividades python

--- unidade7/python/introducion-a-python_web/content/resources/actividade7.py
+++ unidade7/python/introducion-a-python_web/content/resources/actividade7.py
@@ -3,8 +3,8 @@ print('?')
 
 ? = int()
 
-if ? > ?: #caso entre 5 e 30
-    if ? < ?:
+if ? >= ?: #caso entre 5 e 30
+    if ? <= ?:
         print('?') #caso entre 5 e 30
     else:
         print('?') #caso maior de 30
