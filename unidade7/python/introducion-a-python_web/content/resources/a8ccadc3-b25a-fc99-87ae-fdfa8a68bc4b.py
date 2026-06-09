756a0e7327c9904cf5c3606510c20328bfaddc61
tecnomgb <sindo@iesgarciabarros.org>
2026-06-09 12:36:55 +0200

    actualizacion

---
+++ unidade7/python/introducion-a-python_web/content/resources/a8ccadc3-b25a-fc99-87ae-fdfa8a68bc4b.py
@@ -0,0 +1,13 @@
+
+letras_posibles_dni=['T','R','W','A','G','M','Y','F','P','D','X','B','N'
+,'J','Z','S','Q','V','H','L','C','K','E']
+
+?
+
+num_dni = ?
+
+resto = ?%?
+
+letra_dni = letras_posibles_dni[?]
+
+?
---
+++ unidade7/python/introducion-a-python_web/content/resources/actividade11.py
@@ -0,0 +1,17 @@
+
+while True:
+
+    num = ?
+    
+    par_impar = ?%?
+    
+    if (? == ?):
+        print ('O número ', ?, 'é par')
+    else:
+        ?
+    
+    continuar = ?('Desexas probar con outro número? (s/n)')
+    
+    if ? != 's':
+        ?
+        break
